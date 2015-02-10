3cf84b643b4fa1c80a65a92ecd51e7cb2580a849
jask8xd <jask8_xd@hotmail.com>
2015-02-09 21:23:37 -0600

    Update actualizarTecnica.php

--- application/views/tecnica/actualizarTecnica.php
+++ application/views/tecnica/actualizarTecnica.php
@@ -1,9 +1,35 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: leviatan
- * Date: 2/7/15
- * Time: 11:27 PM
- */
 
-para hacer
+<!-- Page Content -->
+<div id="page-wrapper">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12">
+                <fieldset>
+                    <h4>
+                        Actualizar Tecnica
+                        <p></p>
+                    </h4>
+                <form action = <?= base_url('tecnica/actualizarTecnica/')."/".$registro->idTecnica ?> method = 'POST'>
+                    <div class="control-group">
+                        <label class="control-label"  for="descripcionTecnica">Email</label>
+                        <div class="controls">
+                            <input type="text" id="descripcionTecnica" name="descripcionTecnica" placeholder="" class="form-control input-lg" value = <?= $registro->descripcionTecnica ?> >
+                            <p></p>
+                        </div>
+                    </div>
+
+                    <div class="control-group">
+                        <div class="controls">
+                            <button class="btn btn-success" type="submit">Actualizar</button>
+                        </div>
+                    </div>
+                </form>
+                </fieldset>
+            </div>
+            <!-- /.col-lg-12 -->
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /.container-fluid -->
+</div>
+<!-- /#page-wrapper -->
