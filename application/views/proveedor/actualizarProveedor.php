66fd6f6731e0879af7cfbcc3ba634797ee8502d8
jask8xd <jask8_xd@hotmail.com>
2015-02-15 16:37:30 -0600

    Update actualizarProveedor.php

--- application/views/proveedor/actualizarProveedor.php
+++ application/views/proveedor/actualizarProveedor.php
@@ -1,9 +1,35 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: leviatan
- * Date: 2/7/15
- * Time: 11:27 PM
- */
 
-para hacer
+<!-- Page Content -->
+<div id="page-wrapper">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12">
+                <fieldset>
+                    <h4>
+                        Actualizar Proveedor
+                        <p></p>
+                    </h4>
+                <form action = <?= base_url('tecnica/actualizarProveedor/')."/".$registro->idProveedor ?> method = 'POST'>
+                    <div class="control-group">
+                        <label class="control-label"  for="descripcionProveedor">Email</label>
+                        <div class="controls">
+                            <input type="text" id="descripcionProveedor" name="descripcionProveedor" placeholder="" class="form-control input-lg" value = <?= $registro->descripcionProveedor ?> >
+                            <p></p>
+                        </div>
+                    </div>
+
+                    <div class="control-group">
+                        <div class="controls">
+                            <button class="btn btn-success" type="submit">Actualizar</button>
+                        </div>
+                    </div>
+                </form>
+                </fieldset>
+            </div>
+            <!-- /.col-lg-12 -->
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /.container-fluid -->
+</div>
+<!-- /#page-wrapper -->
